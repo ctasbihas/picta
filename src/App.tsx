10ad42d3fcec46fdeeac21ff3de80ea90b4766de
ctasbihas <ctasbihas@gmail.com>
2023-10-28 07:59:50 +0600

    setting up react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./globals.css";
 
 const App = () => {
-	return <h1>Hello Picta</h1>;
+	return (
+		<main className="flex h-screen">
+			<Routes>
+				{/* Public Routes */}
+				<Route
+					path="/sign-in"
+					element={<SigninForm />}
+				/>
+
+				{/* Private Routes */}
+				<Route
+					index
+					element={<Home />}
+				/>
+			</Routes>
+		</main>
+	);
 };
 
 export default App;
