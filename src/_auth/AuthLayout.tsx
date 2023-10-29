3ff7b33892a50fcee581f779536169b66f681d43
ctasbihas <ctasbihas@gmail.com>
2023-10-29 23:11:24 +0600

    Singup page created

--- src/_auth/AuthLayout.tsx
+++ src/_auth/AuthLayout.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import { Outlet, Navigate } from "react-router-dom";
 
 const AuthLayout = () => {
-	return <div>AuthLayout</div>;
+	const isAuthenticated = false;
+
+	return (
+		<>
+			{isAuthenticated ? (
+				<Navigate to="/" />
+			) : (
+				<>
+					<section className="flex flex-1 items-center justify-center flex-col py-10">
+						<Outlet />
+					</section>
+					<img
+						src="/assets/images/side-img.svg"
+						alt=""
+						className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+					/>
+				</>
+			)}
+		</>
+	);
 };
 
 export default AuthLayout;
